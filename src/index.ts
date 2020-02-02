a28f6e0efcccd980624ebc2184683a7df4738235
Robert 'RunAge' Gajda <runage@cloak.kamide.re>
2020-02-02 12:12:57 +0100

    fix: memory leak for standalone

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 import { Client } from 'discord.js';
 import { config as initEnv } from 'dotenv';
-import Tracker, {TrackedUser} from './Tracker';
+import Tracker, { TrackedUser } from './Tracker';
+import { Subscription } from 'rxjs';
 initEnv()
 
 const client = new Client();
+// eslint-disable-next-line functional/no-let
+let subscription: Subscription; 
 
 client
   .on('ready', () => {
     console.log('Ready for tracking!');
-    client.user.setPresence({
-      game: {
-        name: 'you!',
-        type: 'WATCHING'
-      }
-    })
-    Tracker(client).subscribe((user: TrackedUser) => {
-      console.log(user);
-    });
+      client.user.setPresence({
+        game: {
+          name: 'you!',
+          type: 'WATCHING'
+        }
+      })
+      subscription = Tracker(client).subscribe((user: TrackedUser) => {
+        console.log(user);
+      });
   });
+
+client.on('disconnect', () => {
+  subscription.unsubscribe();
+})
 
 client.login(process.env.DSC_TOKEN);
